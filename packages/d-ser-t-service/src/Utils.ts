4086dfb9b45cd8aa41e77ef57183d6f40ddf4490
Katie Prochilo <43346846+KatieProchilo@users.noreply.github.com>
2019-07-30 11:16:27 -0700

    Prettier d-ser-t-service

--- packages/d-ser-t-service/src/Utils.ts
+++ packages/d-ser-t-service/src/Utils.ts
@@ -11,26 +11,35 @@ export default class Utils {
     static readJSONFileSync = (filePath: string): object => {
         let json: object = {};
         try {
-            const data = fs.readFileSync(path.resolve(__dirname, filePath), 'utf8');
+            const data = fs.readFileSync(
+                path.resolve(__dirname, filePath),
+                'utf8'
+            );
             json = JSON.parse(data);
         } catch {
             console.log(`Could not open ${filePath} for the STT service . . .`);
         } finally {
-            console.log('Creation Success!!')
+            console.log('Creation Success!!');
         }
         return json;
-    }
+    };
 
     /**
      * Overwrite the contents of some JSON file, or add content to a new JSON
      * file.
      */
-    static writeJSONFileSync = (filePath: string, data: UnhandledCharacters): void => {
-        fs.writeFileSync(path.resolve(__dirname, filePath), JSON.stringify(data));
-    }
+    static writeJSONFileSync = (
+        filePath: string,
+        data: UnhandledCharacters
+    ): void => {
+        fs.writeFileSync(
+            path.resolve(__dirname, filePath),
+            JSON.stringify(data)
+        );
+    };
 
     /**
-     * Extract regex pattern. /\\s's\\b/g" --> \\s's\\b. If no regex pattern 
+     * Extract regex pattern. /\\s's\\b/g" --> \\s's\\b. If no regex pattern
      * exists, return undefined.
      * @returns regex pattern or undefined if none found
      * @param input string
@@ -39,12 +48,13 @@ export default class Utils {
         if (input.length < 2) {
             console.warn(`${input} is not a regex pattern`);
         }
-        if (input.charAt(0) === `/` &&
+        if (
+            input.charAt(0) === `/` &&
             input.charAt(input.length - 2) === `/` &&
-            input.charAt(input.length - 1) === `g`) {
-
+            input.charAt(input.length - 1) === `g`
+        ) {
             return input.substr(1, input.length - 3);
         }
         return undefined;
-    }
+    };
 }
--- packages/d-ser-t-service/src/main.ts
+++ packages/d-ser-t-service/src/main.ts
@@ -1,58 +1,69 @@
+import fs from 'fs';
+import path from 'path';
 
+import { ITranscriptionAnalyzer } from './interfaces/ITranscriptionAnalyzer';
 import { ResponseAnalyzer } from './ResponseAnalyzer';
+import TranscriptionAnalyzerFactory from './TranscriptionAnalyzerFactory';
 import { TranscriptionFileService } from './TranscriptionFileService';
 import { TranscriptionService } from './TranscriptionService';
-import { TestData, TranscriptionServiceConfig, TestResult, HarnessConfig } from './types';
-import path from 'path';
-import { ITranscriptionAnalyzer } from './interfaces/ITranscriptionAnalyzer';
-import TranscriptionAnalyzerFactory from './TranscriptionAnalyzerFactory';
-import fs from 'fs';
+import {
+    HarnessConfig,
+    TestData,
+    TestResult,
+    TranscriptionServiceConfig,
+} from './types';
+
 export class CustomSpeechTestHarness {
-    private exceptions?: string;
     private audioDirectory?: string;
     private concurrency?: string;
     private crisEndpointId?: string;
+    private exceptions?: string;
+    private localFileService!: TranscriptionFileService;
     private outFile: string;
+    private responseAnalyzer!: ResponseAnalyzer;
     private serviceRegion: string;
     private singleFile?: string;
     private subscriptionKey: string;
+    private transcriptAnalyzer!: ITranscriptionAnalyzer;
     private transcriptionFile?: string;
     private transcriptionService!: TranscriptionService;
-    private localFileService!: TranscriptionFileService;
-    private transcriptAnalyzer!: ITranscriptionAnalyzer;
-    private responseAnalyzer!: ResponseAnalyzer;
-
 
     public constructor(harnessConfig: HarnessConfig) {
         this.audioDirectory = harnessConfig.audioDirectory;
         this.concurrency = harnessConfig.concurrentCalls;
         this.crisEndpointId = harnessConfig.endpointId;
-        this.outFile = harnessConfig.outFile || path.join('.', 'test_results.json');
+        this.exceptions = harnessConfig.exceptions;
+        this.outFile =
+            harnessConfig.outFile || path.join('.', 'test_results.json');
         this.serviceRegion = harnessConfig.region;
         this.singleFile = harnessConfig.audioFile;
         this.subscriptionKey = harnessConfig.subscriptionKey;
         this.transcriptionFile = harnessConfig.transcriptionFile;
-        this.exceptions = harnessConfig.exceptions;
     }
 
     public setTranscriptionService() {
         const config: TranscriptionServiceConfig = {
             subscriptionKey: this.subscriptionKey,
             serviceRegion: this.serviceRegion,
-            endpointID: this.crisEndpointId
+            endpointID: this.crisEndpointId,
         };
 
         this.transcriptionService = new TranscriptionService(config);
     }
+
     public setLocalServices() {
         this.localFileService = new TranscriptionFileService();
-        this.transcriptAnalyzer = TranscriptionAnalyzerFactory.createTranscriptionAnalyzer(this.exceptions);
+        this.transcriptAnalyzer = TranscriptionAnalyzerFactory.createTranscriptionAnalyzer(
+            this.exceptions
+        );
         this.responseAnalyzer = new ResponseAnalyzer(this.transcriptAnalyzer);
     }
+
     public async singleFileTranscription() {
         this.setTranscriptionService();
         if (this.transcriptionService && this.singleFile) {
-            await this.transcriptionService.singleFileTranscribe(this.singleFile)
+            await this.transcriptionService
+                .singleFileTranscribe(this.singleFile)
                 .then(resp => console.log(resp.text))
                 .catch(err => console.warn(err))
                 .finally(() => process.exit(1));
@@ -62,48 +73,78 @@ export class CustomSpeechTestHarness {
     public checkConcurrency() {
         if (!!this.concurrency === false) {
             this.concurrency = '1';
-            const warnMsg = `\nSetting concurrent calls to 1, you can set concurrent calls to service with "-c".\n`
+            const warnMsg = `\nSetting concurrent calls to 1, you can set concurrent calls to service with "-c".\n`;
             console.warn(warnMsg);
         }
     }
 
     public validateAndCleanTranscription(parsedData: TestData) {
         for (const testDatum of parsedData) {
-            testDatum.transcription = this.transcriptAnalyzer.cleanTranscription(testDatum.transcription);
-            this.transcriptAnalyzer.validateExpectedTranscription(testDatum.transcription);
+            testDatum.transcription = this.transcriptAnalyzer.cleanTranscription(
+                testDatum.transcription
+            );
+            this.transcriptAnalyzer.validateExpectedTranscription(
+                testDatum.transcription
+            );
         }
     }
 
     public async multipleFileTranscription() {
         this.setTranscriptionService();
         this.setLocalServices();
-        if (this.transcriptionService && this.transcriptionFile && this.audioDirectory) {
+        if (
+            this.transcriptionService &&
+            this.transcriptionFile &&
+            this.audioDirectory
+        ) {
             const startTime = process.hrtime();
             this.checkConcurrency();
 
-            const parsedData: TestData = this.localFileService.createTestData(this.transcriptionFile, this.audioDirectory);
+            const parsedData: TestData = this.localFileService.createTestData(
+                this.transcriptionFile,
+                this.audioDirectory
+            );
             this.validateAndCleanTranscription(parsedData);
 
-            await this.transcriptionService.batchTranscribe(parsedData, parseInt(this.concurrency!))
-                    // Time it took to parse, validate, and batch transcribe all
-                    // the audio and transcription files.
+            await this.transcriptionService.batchTranscribe(
+                parsedData,
+                parseInt(this.concurrency!)
+            );
+            // Time it took to parse, validate, and batch transcribe all the
+            // audio and transcription files.
             const endTime = process.hrtime(startTime);
 
-            const results = this.transcriptionService!.resultArray.map((item, idx) => {
-                console.log(`Handling result ${idx + 1}/${this.transcriptionService!.resultArray.length} . . .`);
-                return this.responseAnalyzer.handleResponse(item.transcription!, JSON.parse(item.data.json));
-            });
-            results.sort((a,b)=> b.wordErrorRate - a.wordErrorRate);
-                    // From 0 to 1, the rate of transcriptions with at least one
-                    // error. An SER of 1 means every utterance was transcribed
-                    // with at least one error. An SER of 0 means every
-                    // utterance was transcribed perfectly.
-            const sentenceErrorRate = this.responseAnalyzer.calculateSER(results);
+            const results = this.transcriptionService!.resultArray.map(
+                (item, idx) => {
+                    console.log(
+                        `Handling result ${idx + 1}/${
+                            this.transcriptionService!.resultArray.length
+                        } . . .`
+                    );
+                    return this.responseAnalyzer.handleResponse(
+                        item.transcription!,
+                        JSON.parse(item.data.json)
+                    );
+                }
+            );
+            results.sort((a, b) => b.wordErrorRate - a.wordErrorRate);
+            // From 0 to 1, the rate of transcriptions with at least one error.
+            // An SER of 1 means every utterance was transcribed with at least
+            // one error. An SER of 0 means every utterance was transcribed
+            // perfectly.
+            const sentenceErrorRate = this.responseAnalyzer.calculateSER(
+                results
+            );
             console.log(`Sentence Error Rate: ${sentenceErrorRate}`);
 
-            const averageWordErrorRate = ((results.map((item: TestResult, idx: number) => {
-                return item.wordErrorRate && item.wordErrorRate > 0 ? item.wordErrorRate : 0
-            }).reduce(this.responseAnalyzer.reducerSum) / results.length) as number).toPrecision(3);
+            const averageWordErrorRate = ((results
+                .map((item: TestResult, idx: number) => {
+                    return item.wordErrorRate && item.wordErrorRate > 0
+                        ? item.wordErrorRate
+                        : 0;
+                })
+                .reduce(this.responseAnalyzer.reducerSum) /
+                results.length) as number).toPrecision(3);
             console.log(`Average Word Error Rate: ${averageWordErrorRate}`);
 
             const totalTestingTime = `${endTime[0]} seconds`;
@@ -111,10 +152,15 @@ export class CustomSpeechTestHarness {
                 transcriptionFile: this.transcriptionFile,
                 sentenceErrorRate,
                 averageWordErrorRate,
-                totalTestingTime
+                totalTestingTime,
             };
 
-            this.outFile ? this.localFileService.writeToTextFile(this.outFile, { metaData, results }) : null;
+            this.outFile
+                ? this.localFileService.writeToTextFile(this.outFile, {
+                      metaData,
+                      results,
+                  })
+                : null;
             console.log(`Runtime: ${totalTestingTime}`);
         }
     }
@@ -123,11 +169,20 @@ export class CustomSpeechTestHarness {
         this.setTranscriptionService();
         if (this.transcriptionService && files) {
             this.checkConcurrency();
-            await this.transcriptionService.batchTranscribe(files, parseInt(this.concurrency!));
-            const results: { index: number, file?: string, transcription: string }[] = this.transcriptionService!.resultArray.map(
-                (item, index) => ({ index, file: item.file, transcription: JSON.parse(item.data.json).NBest[0].Lexical })
+            await this.transcriptionService.batchTranscribe(
+                files,
+                parseInt(this.concurrency!)
             );
+            const results: {
+                index: number;
+                file?: string;
+                transcription: string;
+            }[] = this.transcriptionService!.resultArray.map((item, index) => ({
+                index,
+                file: item.file,
+                transcription: JSON.parse(item.data.json).NBest[0].Lexical,
+            }));
             return results;
         }
-    };
+    }
 }
