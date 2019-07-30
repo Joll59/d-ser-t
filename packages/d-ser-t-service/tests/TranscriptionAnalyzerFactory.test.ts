4de58e5a1ed988b26ab878f0d7659d803b2a377a
Katie Prochilo <43346846+KatieProchilo@users.noreply.github.com>
2019-07-30 11:16:27 -0700

    Prettier d-ser-t-service

--- packages/d-ser-t-service/tests/TranscriptionAnalyzerFactory.test.ts
+++ packages/d-ser-t-service/tests/TranscriptionAnalyzerFactory.test.ts
@@ -1,22 +1,21 @@
-import {
-    TranscriptionAnalyzerCI
-} from '../src/TranscriptionAnalyzerCI';
-
-import {
-    TranscriptionAnalyzer
-} from '../src/TranscriptionAnalyzer';
-
+import { TranscriptionAnalyzer } from '../src/TranscriptionAnalyzer';
+import { TranscriptionAnalyzerCI } from '../src/TranscriptionAnalyzerCI';
 import TranscriptionAnalyzerFactory from '../src/TranscriptionAnalyzerFactory';
 
 const configFile: string = `../CiCleanUpConfig.json`;
 
-describe("Can return the right instance", () => {
-    it("should return the CI object", () => {
-        expect(TranscriptionAnalyzerFactory.createTranscriptionAnalyzer(configFile) instanceof TranscriptionAnalyzerCI)
-            .toEqual(true);
+describe('Can return the right instance', () => {
+    it('should return the CI object', () => {
+        expect(
+            TranscriptionAnalyzerFactory.createTranscriptionAnalyzer(
+                configFile
+            ) instanceof TranscriptionAnalyzerCI
+        ).toEqual(true);
     });
-    it("should return transcriptAnalyzer", () => {
-        expect(TranscriptionAnalyzerFactory.createTranscriptionAnalyzer() instanceof TranscriptionAnalyzer)
-            .toEqual(true);
+    it('should return transcriptAnalyzer', () => {
+        expect(
+            TranscriptionAnalyzerFactory.createTranscriptionAnalyzer() instanceof
+                TranscriptionAnalyzer
+        ).toEqual(true);
     });
 });
