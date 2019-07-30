061f01dc57a711770120c50ecea5b1bdd294a5a6
Katie Prochilo <43346846+KatieProchilo@users.noreply.github.com>
2019-07-30 11:30:04 -0700

    Prettier d-ser-t-cli

--- packages/d-ser-t-cli/src/main.ts
+++ packages/d-ser-t-cli/src/main.ts
@@ -1,8 +1,7 @@
-// // import cli from './cli';
 import { CustomSpeechTestHarness } from 'd-ser-t-service';
-import { HarnessConfig } from 'd-ser-t-service/lib/types'
-export const start = async (harnessArgs: HarnessConfig) => {
+import { HarnessConfig } from 'd-ser-t-service/lib/types';
 
+export const start = async (harnessArgs: HarnessConfig) => {
     const harness = new CustomSpeechTestHarness(harnessArgs);
     if (harnessArgs.audioFile) {
         harness.singleFileTranscription();
