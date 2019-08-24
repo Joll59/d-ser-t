891e2f21dedfe32dbf07481d53b761a77747d66d
nairmai <mai.nair34@gmail.com>
2019-08-23 17:08:52 -0700

    Begin testing framework

--- packages/d-ser-t-service/tests/XmlWriterService.test.ts
+++ packages/d-ser-t-service/tests/XmlWriterService.test.ts
@@ -1,2 +1,16 @@
 import * as path from 'path';
 import { XmlWriterService } from '../src/index';
+
+describe('XMLWriterService', () => {
+    describe('countNumFailures', () => {
+        it('returns the correct number of failures', () => {
+            // TODO
+        });
+    });
+
+    describe('getNumericalTime', () => {
+        it('returns the correct time as a Number', () => {
+            // TODO
+        });
+    });
+});
