77dbfe00b1103b55f7533d1adcb517cd0824aedc
Katie Prochilo <43346846+KatieProchilo@users.noreply.github.com>
2019-09-10 17:40:34 -0700

    WER moved from failure element to testcase.

--- packages/d-ser-t-service/tests/XmlWriterService.test.ts
+++ packages/d-ser-t-service/tests/XmlWriterService.test.ts
@@ -1,5 +1,5 @@
 import * as fs from 'fs';
-import { isNumber } from 'util';
+
 import { TestMetaData, TestResult } from '../src/types';
 import { XmlWriterService } from '../src/XmlWriterService';
 
@@ -40,30 +40,20 @@ afterAll(() => {
 describe('XMLWriterService', () => {
     describe('countNumFailures', () => {
         it('returns the correct number of failures', () => {
-            const numFailures = xws.countNumFailures(mockResults);
+            const numFailures = xws.getFailureCount(mockResults);
             expect(numFailures).toEqual(1);
-        });
-    });
-
-    describe('getNumericalTime', () => {
-        const time = xws.getNumericalTime(mockMetaData);
-        it('returns a Number', () => {
-            expect(isNumber(time)).toBe(true);
-        });
-        it('returns the correct time', () => {
-            expect(time).toEqual(10);
         });
     });
 
     describe('toJUnitXml', () => {
         const expected =
             `<?xml version="1.0" encoding="utf-8"?>\n` +
-            `<testsuites name="CRIS STT tests" tests="2" failures="1" time="10" avg_SER="0.5" avg_WER="0.5">\n` +
-            `  <testsuite name="test_transcription_file.txt" errors="0" failures="1" skipped="0" timestamp="2019-01-01 00:00:00" time="10" tests="2" SER="0.5" avg_WER="0.5">\n` +
-            `    <testcase classname="test-1" name="test_transcription_file.txt" time="n/a" expected="alright">\n` +
-            `      <failure actual="all right" WER="1"/>\n` +
+            `<testsuites name="CRIS STT tests" tests="2" failures="1" time="10" SER="0.5" AWER="0.5">\n` +
+            `  <testsuite name="test_transcription_file.txt" tests="2" failures="1" time="10" timestamp="2019-01-01 00:00:00" errors="0" skipped="0" SER="0.5" AWER="0.5">\n` +
+            `    <testcase classname="test-1" WER="1" expected="alright" name="N/A" time="N/A">\n` +
+            `      <failure actual="all right"/>\n` +
             `    </testcase>\n` +
-            `    <testcase classname="test-2" name="test_transcription_file.txt" time="n/a" expected="hi"/>\n` +
+            `    <testcase classname="test-2" WER="0" expected="hi" name="N/A" time="N/A"/>\n` +
             `  </testsuite>\n` +
             `</testsuites>`;
 
